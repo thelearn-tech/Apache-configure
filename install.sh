8f9d960e84b75ed321984e7592670f4d1b203c87
Kyler <46557883+HiFiiDev@users.noreply.github.com>
2021-01-09 02:19:26 -0700

    Work in progress! Update install.sh
    Add a little more to the script, etc.
    
    Let me know what you think so far

--- install.sh
+++ install.sh
@@ -1,26 +1,45 @@
-printf '\033[36m';echo Installation script by
-echo thelearn-tech 
-echo AKA Pritam Behera
-sleep 2
 clear
-echo
-echo
-printf '\033[32m';echo auto configuring http.conf
-cd /data/data/com.termux/files/home
-mkdir host
-cd /data/data/com.termux/files/usr/etc/apache2
-mv httpd.conf /data/data/com.termux/files/home
-cd
-cd Apache-configure
-mv httpd.conf /data/data/com.termux/files/usr/etc/apache2
+printf '\033[32m';echo "Hello! "
+sleep 1.5
+echo "This is a script to download Apache2, you can run your own web server. "
+echo " "
+sleep 1.6
+printf '\033[36m';echo "Installation script by thelearn-tech"
+sleep 1.5
+echo "AKA Pritam Behera "
 sleep 2
-clear 
-printf '\033[32m';echo installation complete
-echo
-sleep 1
-echo for more details visit my github
-echo
-printf '\033[36m';echo https://github.com/thelearn-tech/Apache-configure
-sleep 3 
-clear
+echo " "
+echo " "
+setup() {
+    echo auto configuring http.conf
+}
+
+folderCheckOne() {
+    cd /data/data/com.termux/files/home/
+    echo "Checking for a hosts file... "
+    if [ ! -d host ]
+    then mkdir host
+        echo "Hosts folder created! "
+    else echo "Hosts folder found. "
+    fi
+}
+
+other() {
+    cd /data/data/com.termux/files/usr/etc/apache2
+    mv httpd.conf /data/data/com.termux/files/home
+    cd
+    cd Apache-configure
+    mv httpd.conf /data/data/com.termux/files/usr/etc/apache2
+    sleep 2
+    clear
+    printf '\033[32m';echo installation complete
+    echo
+    sleep 1
+    echo for more details visit my github
+    echo
+    printf '\033[36m';echo https://github.com/thelearn-tech/Apache-configure
+    sleep 3
+    clear
+}
+folderCheckOne
 exit 1
